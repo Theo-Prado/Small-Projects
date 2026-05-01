f876bc77d1f474fe8bd60491252c60509a6d2695
Theo <theo.silva.prado@gmail.com>
2026-05-01 15:49:09 -0300

    Add script for discovering new territories

---
+++ DESCOBERTA-BRASIL.py
@@ -0,0 +1,18 @@
+novos_territorios = []
+continentes = ["Europa","Africa","Asia"]
+
+terra_a_vista = True
+
+if terra_a_vista:
+  novos_territorios.append("Brasil")
+  continentes.append("América")
+  print("Aqui é a Índia? Espera... não! É a Ilha de Vera Cruz!")
+else:
+  print("Chegamos a Índia! Finalmente!")
+print()
+print(novos_territorios)
+print()
+for continente in continentes:
+  print(continente)
+
+print("Feliz dia da, entre aspas, descoberta do Brasil!")
